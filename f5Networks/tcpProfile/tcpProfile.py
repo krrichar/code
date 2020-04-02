e0388375ceacb75136cedebdc3e20c7752d9ba37
krrichar <61367889+krrichar@users.noreply.github.com>
2020-04-01 23:25:26 -0400

    Update tcpProfile.py

--- f5Networks/tcpProfile/tcpProfile.py
+++ f5Networks/tcpProfile/tcpProfile.py
@@ -6,30 +6,33 @@ import requests
 import urllib3
 
 PASSWORD = getpass()
-HOSTS = ['ltm-1a']
+HOSTS = [
+    'us6647ny-ppcore-ltm-n2a',
+    'us6647ny-ppcore-ltm-n',
+    'us6647ny-ppcore-ltm-perf',
+    'us6645ny-ppcore-ltm-n2a',
+    'us6645ny-ppcore-ltm-n',
+    'us6645ny-ppcore-ltm-perf'
+]
 
 for host in HOSTS:
     urllib3.disable_warnings()
     url = f'https://{host}/mgmt/tm/ltm/profile/tcp'
     payload = \
-        '''
-    {
-        "name": "tcp-client-custom-1800",
-        "description": "idle timeout 1800",
-        "defaultsFrom": "tcp-lan-optimized",
-        "idleTimeout": 1800,
-        "keepAliveInterval": 3600
-    }
-        '''
+        {
+            "name": "tcplanoptimizedclientpf-alb-parent-custom-1800",
+            "description": "idle timeout 1800",
+            "defaultsFrom": "tcplanoptimizedclientpf-alb-parent",
+            "idleTimeout": 1800,
+            "keepAliveInterval": 3600
+        }
     headers = {'Content-Type': 'application/json'}
-    response = requests.request(
-        'POST',
+    response = requests.post(
         url,
-        headers=headers,
+        headers={'Content-Type': 'application/json'},
         auth=('admin', PASSWORD),
-        data=payload,
-        verify=False,
-        )
-    print(f'\n{host}:'.upper())
+        data=json.dumps(payload),
+        verify=False
+    )
+    print(f'\n{host}:'.upper(), 'Code:', response.status_code)
     print(response.text.encode('utf8'))
-
